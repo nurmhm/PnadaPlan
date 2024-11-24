18fefeaec658174b0b18db975db465502d078e2e
Nur Mohammad <nurmd7228@gmail.com>
2024-11-24 12:00:54 +0100

    project setup done

---
+++ src/components/custom/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <img src="/pandaPlan.png" alt="panda plan logo"  width={100}/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router'
+import CreateTrip from './Create-trip/Index'
+import Header from './components/custom/Header'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/create-trip",
+    element: <CreateTrip/>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Header/>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
